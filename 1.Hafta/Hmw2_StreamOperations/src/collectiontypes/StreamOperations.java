be5cd9b7e2a88330758111db194518906e5e32d1
tunahan burak dirlik <87035514+tburakdirlik@users.noreply.github.com>
2021-12-26 18:22:51 +0300

    Update StreamOperations.java

--- 1.Hafta/Hmw2_StreamOperations/src/collectiontypes/StreamOperations.java
+++ 1.Hafta/Hmw2_StreamOperations/src/collectiontypes/StreamOperations.java
@@ -70,7 +70,7 @@ public class StreamOperations {
 
     public static void main(String[] args) {
 
-        //Collection data structures creaded below
+        //Collection data structures created below
         Collection<Integer> collection1 = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         Collection<Integer> collection2 = Arrays.asList(9, 3, 2 , 2);
         List<Integer> collection = Arrays.asList(9, 3, 2);
