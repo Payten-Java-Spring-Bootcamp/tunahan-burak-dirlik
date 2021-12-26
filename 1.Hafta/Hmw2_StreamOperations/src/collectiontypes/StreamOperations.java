6fef78729697947f77ea2ab864635e6af0693171
tunahan burak dirlik <87035514+tburakdirlik@users.noreply.github.com>
2021-12-26 17:24:04 +0300

    Update StreamOperations.java

--- 1.Hafta/Hmw2_StreamOperations/src/collectiontypes/StreamOperations.java
+++ 1.Hafta/Hmw2_StreamOperations/src/collectiontypes/StreamOperations.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 
 public class StreamOperations {
 
+    //Collection concat method
     public static <T> Collection<T> concat(Collection<T> first, Collection<T> second) {
 
         Collection<T> concatenated = Stream
@@ -16,13 +17,16 @@ public class StreamOperations {
 
         return concatenated;
     }
-
+    
+    
+    //Difference method: Returns the elements in the first collection that the second collection is not in 
     public static <T> Stream<T> difference(Collection<T> first, Collection<T> second) {
 
         Stream<T> stream = first.stream();
         return stream.filter(X -> !(second.contains(X)));
     }
 
+    //Distinct method: Returns a new stream list with any duplicates removed.
     public static <T> Stream<T> distinct(Collection<T> first) {
 
         Stream<T> c = first.stream()
@@ -32,6 +36,7 @@ public class StreamOperations {
 
     }
 
+    //Slice method: Return a sub-sequence of the list between the given first and last positions.
     public static <T> Stream slice(Collection<T> first, int firstindex, int lastindex) {
         Stream<T> stream = first.stream();
         Stream.iterate(firstindex, n -> n + 1)
@@ -43,10 +48,12 @@ public class StreamOperations {
 
     public static void main(String[] args) {
 
+        //Collection data structures creaded below
         Collection<Integer> collection1 = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         Collection<Integer> collection2 = Arrays.asList(9, 3, 2);
         List<Integer> collection = Arrays.asList(9, 3, 2);
 
+        //Lets try these methods
         System.out.println(concat(collection1, collection2));
         System.out.println(difference(collection1, collection).collect(Collectors.toList()));
         System.out.println(distinct(collection1).collect(Collectors.toList()));System.out.println("--------------");
